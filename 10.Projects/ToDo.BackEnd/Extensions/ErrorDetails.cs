074380f088160e627135044466a3db151e9883de
Gustavo Pilar <gustavo.silva@weipa.com.br>
2026-01-06 11:22:11 -0300

    Handle global exceptions using middleware and extensions

---
+++ 10.Projects/ToDo.BackEnd/Extensions/ErrorDetails.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json;
+
+namespace ToDo.BackEnd
+{
+    public class ErrorDetails
+    {
+        public int StatusCode { get; set; }
+        public string? Message { get; set; }
+        public string? Trace { get; set; }
+        public string ToString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+    }
+}
